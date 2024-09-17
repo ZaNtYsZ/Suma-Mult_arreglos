1db9846abb072245467dfc4d375018c87e9ecdac
ZaNtYsZ <ssmena@uamv.edu.ni>
2024-09-16 23:37:11 -0600

    Ejercicio3

---
+++ Ejercicio 3/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿using System.Windows.Forms;
+using System;
+
+namespace Ejercicio_3
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtVendedor = new System.Windows.Forms.TextBox();
+            this.txtProducto = new System.Windows.Forms.TextBox();
+            this.txtVenta = new System.Windows.Forms.TextBox();
+            this.lblVendedor = new System.Windows.Forms.Label();
+            this.lblProducto = new System.Windows.Forms.Label();
+            this.lblVenta = new System.Windows.Forms.Label();
+            this.btnAgregarVenta = new System.Windows.Forms.Button();
+            this.btnMostrarReporte = new System.Windows.Forms.Button();
+            this.lstReporte = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtVendedor
+            // 
+            this.txtVendedor.Location = new System.Drawing.Point(186, 6);
+            this.txtVendedor.Name = "txtVendedor";
+            this.txtVendedor.Size = new System.Drawing.Size(100, 20);
+            this.txtVendedor.TabIndex = 1;
+            // 
+            // txtProducto
+            // 
+            this.txtProducto.Location = new System.Drawing.Point(186, 35);
+            this.txtProducto.Name = "txtProducto";
+            this.txtProducto.Size = new System.Drawing.Size(100, 20);
+            this.txtProducto.TabIndex = 2;
+            // 
+            // txtVenta
+            // 
+            this.txtVenta.Location = new System.Drawing.Point(157, 68);
+            this.txtVenta.Name = "txtVenta";
+            this.txtVenta.Size = new System.Drawing.Size(100, 20);
+            this.txtVenta.TabIndex = 3;
+            // 
+            // lblVendedor
+            // 
+            this.lblVendedor.AutoSize = true;
+            this.lblVendedor.Location = new System.Drawing.Point(12, 9);
+            this.lblVendedor.Name = "lblVendedor";
+            this.lblVendedor.Size = new System.Drawing.Size(167, 13);
+            this.lblVendedor.TabIndex = 4;
+            this.lblVendedor.Text = "Número de Vendedor (1 a 4)";
+            this.lblVendedor.Click += new System.EventHandler(this.lblVendedor_Click);
+            // 
+            // lblProducto
+            // 
+            this.lblProducto.AutoSize = true;
+            this.lblProducto.Location = new System.Drawing.Point(12, 38);
+            this.lblProducto.Name = "lblProducto";
+            this.lblProducto.Size = new System.Drawing.Size(164, 13);
+            this.lblProducto.TabIndex = 5;
+            this.lblProducto.Text = "Número de Producto (1 a 5)";
+            // 
+            // lblVenta
+            // 
+            this.lblVenta.AutoSize = true;
+            this.lblVenta.Location = new System.Drawing.Point(11, 68);
+            this.lblVenta.Name = "lblVenta";
+            this.lblVenta.Size = new System.Drawing.Size(130, 13);
+            this.lblVenta.TabIndex = 6;
+            this.lblVenta.Text = "Monto de la Venta ($)";
+            this.lblVenta.Click += new System.EventHandler(this.lblVenta_Click);
+            // 
+            // btnAgregarVenta
+            // 
+            this.btnAgregarVenta.Image = ((System.Drawing.Image)(resources.GetObject("btnAgregarVenta.Image")));
+            this.btnAgregarVenta.Location = new System.Drawing.Point(64, 365);
+            this.btnAgregarVenta.Name = "btnAgregarVenta";
+            this.btnAgregarVenta.Size = new System.Drawing.Size(55, 49);
+            this.btnAgregarVenta.TabIndex = 7;
+            this.btnAgregarVenta.UseVisualStyleBackColor = true;
+            this.btnAgregarVenta.Click += new System.EventHandler(this.btnAgregarVenta_Click);
+            // 
+            // btnMostrarReporte
+            // 
+            this.btnMostrarReporte.Image = ((System.Drawing.Image)(resources.GetObject("btnMostrarReporte.Image")));
+            this.btnMostrarReporte.Location = new System.Drawing.Point(277, 365);
+            this.btnMostrarReporte.Name = "btnMostrarReporte";
+            this.btnMostrarReporte.Size = new System.Drawing.Size(66, 49);
+            this.btnMostrarReporte.TabIndex = 8;
+            this.btnMostrarReporte.UseVisualStyleBackColor = true;
+            this.btnMostrarReporte.Click += new System.EventHandler(this.btnMostrarReporte_Click);
+            // 
+            // lstReporte
+            // 
+            this.lstReporte.FormattingEnabled = true;
+            this.lstReporte.Location = new System.Drawing.Point(12, 118);
+            this.lstReporte.Name = "lstReporte";
+            this.lstReporte.Size = new System.Drawing.Size(300, 199);
+            this.lstReporte.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 344);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Registrar Venta/Añadir Venta";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(255, 344);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Generar Reporte";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(400, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstReporte);
+            this.Controls.Add(this.btnMostrarReporte);
+            this.Controls.Add(this.btnAgregarVenta);
+            this.Controls.Add(this.lblVenta);
+            this.Controls.Add(this.lblProducto);
+            this.Controls.Add(this.lblVendedor);
+            this.Controls.Add(this.txtVenta);
+            this.Controls.Add(this.txtProducto);
+            this.Controls.Add(this.txtVendedor);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Resumen de Ventas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.TextBox txtVendedor;
+        private System.Windows.Forms.TextBox txtProducto;
+        private System.Windows.Forms.TextBox txtVenta;
+        private System.Windows.Forms.Label lblVendedor;
+        private System.Windows.Forms.Label lblProducto;
+        private System.Windows.Forms.Label lblVenta;
+        private System.Windows.Forms.Button btnAgregarVenta;
+        private System.Windows.Forms.Button btnMostrarReporte;
+        private System.Windows.Forms.ListBox lstReporte;
+        private Label label1;
+        private Label label2;
+    }
+}
