37cff84b3f606594b91f2f7b4f42944ddb0cf2f7
ZaNtYsZ <ssmena@uamv.edu.ni>
2024-09-16 23:17:45 -0600

    ejercicio2

---
+++ Ejercicio 2/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Ejercicio_2
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.rdoSmoking = new System.Windows.Forms.RadioButton();
+            this.rdoNonSmoking = new System.Windows.Forms.RadioButton();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblBoardingPass = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Unispace", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(310, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Elija su opción de viaje:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // rdoSmoking
+            // 
+            this.rdoSmoking.AutoSize = true;
+            this.rdoSmoking.Location = new System.Drawing.Point(93, 49);
+            this.rdoSmoking.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rdoSmoking.Name = "rdoSmoking";
+            this.rdoSmoking.Size = new System.Drawing.Size(140, 20);
+            this.rdoSmoking.TabIndex = 1;
+            this.rdoSmoking.TabStop = true;
+            this.rdoSmoking.Text = "Area para fumar ";
+            this.rdoSmoking.UseVisualStyleBackColor = true;
+            // 
+            // rdoNonSmoking
+            // 
+            this.rdoNonSmoking.AutoSize = true;
+            this.rdoNonSmoking.Location = new System.Drawing.Point(306, 49);
+            this.rdoNonSmoking.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rdoNonSmoking.Name = "rdoNonSmoking";
+            this.rdoNonSmoking.Size = new System.Drawing.Size(127, 20);
+            this.rdoNonSmoking.TabIndex = 2;
+            this.rdoNonSmoking.TabStop = true;
+            this.rdoNonSmoking.Text = "No Fumadores";
+            this.rdoNonSmoking.UseVisualStyleBackColor = true;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(136, 149);
+            this.btnSubmit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(249, 30);
+            this.btnSubmit.TabIndex = 3;
+            this.btnSubmit.Text = "Asignar Area";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblBoardingPass
+            // 
+            this.lblBoardingPass.AutoSize = true;
+            this.lblBoardingPass.Location = new System.Drawing.Point(57, 204);
+            this.lblBoardingPass.Name = "lblBoardingPass";
+            this.lblBoardingPass.Size = new System.Drawing.Size(0, 16);
+            this.lblBoardingPass.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(136, 90);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(34, 33);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(350, 90);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(35, 33);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(534, 335);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblBoardingPass);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.rdoNonSmoking);
+            this.Controls.Add(this.rdoSmoking);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Preferencia de viaje";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rdoSmoking;
+        private System.Windows.Forms.RadioButton rdoNonSmoking;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblBoardingPass;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
